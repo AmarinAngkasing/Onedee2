ebf2d73a03a23042a34cfb32ccd0737c60da2d1d
Amarin <angkasing@gmail.com>
2022-07-10 11:10:30 +0700

    a3

---
+++ lib/states/list_document.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListDocument extends StatefulWidget {
+  @override
+  _ListDocumentState createState() => _ListDocumentState();
+}
+
+class _ListDocumentState extends State<ListDocument> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/states/pdf_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyPdfView extends StatefulWidget {
+  @override
+  _MyPdfViewState createState() => _MyPdfViewState();
+}
+
+class _MyPdfViewState extends State<MyPdfView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
